334d135b11a5201f23a8a7496d0fbcdf7a973c10
insurancebyjack <hello@insurancebyjack.co.uk>
2015-07-06 15:57:07 +0100

    Property validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,13 @@
 class Property < ActiveRecord::Base
+	validates 	:postcode, :city, :presence => true
+
+	validate :at_least_one_street_address_must_be_present
+
+	private
+
+	def at_least_one_street_address_must_be_present
+		if (street_address.blank? and street_address_two.blank?)
+			errors.add(:base, "At least one street address must be present")
+		end
+	end
 end
