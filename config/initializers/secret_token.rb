de848bd2168a1dec44ab8e0685bea4561a279c1f
insurancebyjack <hello@insurancebyjack.co.uk>
2015-07-06 13:54:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lodgerapp::Application.config.secret_key_base = '5a4609bb328990d47ab22503b96f65b7555b87adf6e9a5cd22568fa02f94aac07a4d4ca549acd1b3829f452ca85cf53d42a93d3f4f41c33b63df27ae6e35bd64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lodgerapp::Application.config.session_store :cookie_store, key: '_lodgerapp_session'
