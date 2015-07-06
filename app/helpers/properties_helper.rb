de5125e32b563145afb2c2a2f64f7210822c3bd1
insurancebyjack <hello@insurancebyjack.co.uk>
2015-07-06 16:32:44 +0100

    Basic views for Property

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,5 @@
 module PropertiesHelper
+	def full_address(property)
+		[property.house_number, property.street_address, property.street_address_two, property.city, property.postcode.upcase,].compact.reject(&:blank?).join(', ')
+	end
 end
