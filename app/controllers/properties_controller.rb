26cbf6710f16dbac964d7c3ddf4d81c149165e54
insurancebyjack <hello@insurancebyjack.co.uk>
2015-07-06 14:41:56 +0100

    Property CRUD

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,45 @@
 class PropertiesController < ApplicationController
+	def index
+		@properties = Property.all
+	end
+
+	def new
+		@property = Property.new
+	end
+
+	def create
+		@property = Property.new(app_params)
+		if @property.save
+			redirect_to @property
+		else
+			render('new')
+		end
+	end
+
+	def show
+		@property = Property.find(params[:id])
+	end
+
+	def edit
+		@property = Property.find(params[:id])
+	end
+
+	def update
+		@property = Property.find(params[:id])
+		if @property.update_attributes(app_params)
+			redirect_to(@property)
+		else
+			render('edit')
+		end
+	end
+
+	def destroy
+		@property = Property.find(params[:id])
+		@property.destroy
+		redirect_to properties_path
+	end
+
+	def app_params
+		params.require(:property).permit(:house_number, :street_address, :street_address_two, :city, :postcode)
+	end
 end
