c6f3b76fdae07627d4c703c90babb1cea3b8ac60
insurancebyjack <hello@insurancebyjack.co.uk>
2015-07-10 14:26:05 +0100

    Tenant validation

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,5 @@
 class Tenant < ActiveRecord::Base
+	validates :first_name, :second_name, :rent_start_date, :presence => true
+	validates :house_phone, :mobile_phone, :length => {:minimum => 9, :maximum => 15}, :format => { :with => /\A\S[0-9\+\/\(\)\s\-]*\z/i }, :allow_blank => true
+	validates :email, :format => { :with => /\A[^@]+@[^@]+\z/ }, :allow_blank => true
 end
