366c0a3dc4e50df6790f823e199474b248bc6a6d
Ashley <ashleybaxter@me.com>
2015-07-14 20:58:49 +0100

    UK date format and currency

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Lodgerapp::Application.initialize!
+
+my_date_formats = { :default => '%d/%m/%Y' } 
+Time::DATE_FORMATS.merge!(my_date_formats) 
+Date::DATE_FORMATS.merge!(my_date_formats)
