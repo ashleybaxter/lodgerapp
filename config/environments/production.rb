996b3e55b27c56e851cf99f679ae04f961269f69
Ashley <ashleybaxter@me.com>
2015-07-27 11:56:21 +0100

    Devise configured for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,15 @@ Lodgerapp::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'lodgerapp.co.uk' }
+  config.action_mailer.smtp_settings = {
+    address: "server2.pcs-net.com",
+    port: 25,
+    domain: "brokersdirect.co.uk",
+    user_name: ENV["EMAIL"],
+    password: ENV["PASSWORD"]
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
