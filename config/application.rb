d080d4a2e6246b30800fd045e14ce279d4f85132
insurancebyjack <hello@insurancebyjack.co.uk>
2015-08-03 14:14:23 +0100

    ActiveAdmin config assets precompile

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Lodgerapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile =  ['*.js', '*.css', '*.css.erb', 'active_admin.css'] 
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,11 @@ Lodgerapp::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+
   # Generate digests for assets URLs.
   config.assets.digest = true
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb', 'active_admin.css'] 
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
